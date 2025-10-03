acf5220376e7584ee014375d8d98b89e189a8f7a
Patrick Kyei <pkyei804@gmail.com>
2025-10-03 20:55:40 +0000

    addPWA

---
+++ service-worker.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = "patrickkyei-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/assets/css/style.css",
+  "/assets/js/main.js",
+  "/assets/img/favicons/favicon-16x16.png"
+];
+
+// Install SW and cache assets
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+// Fetch assets
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Update cache
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      );
+    })
+  );
+});
