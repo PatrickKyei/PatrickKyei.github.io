d316926228bc63c10e84ec610c3113fba14678c4
Patrick Kyei <pkyei804@gmail.com>
2025-10-03 22:05:48 +0000

    addPWA

--- service-worker.js
+++ service-worker.js
@@ -1,37 +1,67 @@
-const CACHE_NAME = "patrickkyei-cache-v1";
-const urlsToCache = [
-  "/",
-  "/index.html",
-  "/assets/css/style.css",
-  "/assets/js/main.js",
-  "/assets/img/favicons/favicon-16x16.png"
-];
+importScripts('/assets/js/data/swconf.js');
 
-// Install SW and cache assets
-self.addEventListener("install", (event) => {
+function verifyUrl(url) {
+  const requestUrl = new URL(url);
+  const requestPath = requestUrl.pathname;
+
+  if (!requestUrl.protocol.startsWith('http')) {
+    return false;
+  }
+
+  for (const prefix of swconf.interceptor.urlPrefixes) {
+    if (requestUrl.href.startsWith(prefix)) {
+      return false;
+    }
+  }
+
+  for (const path of swconf.interceptor.paths) {
+    if (requestPath.startsWith(path)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+self.addEventListener('install', (event) => {
+  if (swconf.purge) return;
+
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+    caches.open(swconf.cacheName).then((cache) => cache.addAll(swconf.resources))
   );
 });
 
-// Fetch assets
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (swconf.purge || key !== swconf.cacheName) {
+            return caches.delete(key);
+          }
+        })
+      );
     })
   );
 });
 
-// Update cache
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames
-          .filter((name) => name !== CACHE_NAME)
-          .map((name) => caches.delete(name))
-      );
+self.addEventListener('fetch', (event) => {
+  if (event.request.headers.has('range')) return;
+
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) return response;
+
+      return fetch(event.request).then((response) => {
+        if (swconf.purge || event.request.method !== 'GET' || !verifyUrl(event.request.url)) {
+          return response;
+        }
+
+        let responseToCache = response.clone();
+        caches.open(swconf.cacheName).then((cache) => {
+          cache.put(event.request, responseToCache);
+        });
+        return response;
+      });
     })
   );
 });
